e2abc236d2c28a88776cfbf3a7eeaab1176194d0
xuchaojie <chaojie.xu@visionacademy.cn>
2024-07-18 11:39:14 +0800

    feat:  更新分布site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://xujiuran.github.io',
+	site: 'https://xujiuran.github.io/tomato.astro/',
   base: 'my-repo',
 	integrations: [mdx(), sitemap(), UnoCSS({ injectReset: true })],
 });
